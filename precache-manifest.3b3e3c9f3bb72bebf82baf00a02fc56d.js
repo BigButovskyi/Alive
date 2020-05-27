aec130e02589f1486cf44a70468d82dd128b0f0e
VladokAR <vladokthrr@gmail.com>
2020-05-28 00:15:13 +0300

    Updates

---
+++ precache-manifest.3b3e3c9f3bb72bebf82baf00a02fc56d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d2917f77c71a79eca610ef223bbcd17",
+    "url": "/Alive/index.html"
+  },
+  {
+    "revision": "b9c10508c5226e527742",
+    "url": "/Alive/static/css/main.55bb8792.chunk.css"
+  },
+  {
+    "revision": "aeb4370c1e3b3f738fe1",
+    "url": "/Alive/static/js/2.dc33dc54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Alive/static/js/2.dc33dc54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9c10508c5226e527742",
+    "url": "/Alive/static/js/main.cf3d5a25.chunk.js"
+  },
+  {
+    "revision": "a23bb1beff35ec54b6c7",
+    "url": "/Alive/static/js/runtime-main.f84e6a70.js"
+  },
+  {
+    "revision": "c7d5266a69b150320a6c545a3b8360d8",
+    "url": "/Alive/static/media/ailerons.c7d5266a.otf"
+  },
+  {
+    "revision": "95fb8b73cd5a536dc95994d13f90a924",
+    "url": "/Alive/static/media/andromeda.95fb8b73.ttf"
+  },
+  {
+    "revision": "ca8632f4d0aacae4633cbfc2f1a673f1",
+    "url": "/Alive/static/media/anurati.ca8632f4.otf"
+  },
+  {
+    "revision": "308ee0f6ee0c7e4d29bfb8993dbdcf9a",
+    "url": "/Alive/static/media/clouds.308ee0f6.png"
+  },
+  {
+    "revision": "4a714e9ec9379b0fca2cf8bc0767aaf4",
+    "url": "/Alive/static/media/man.4a714e9e.png"
+  },
+  {
+    "revision": "0e92819357c7ef35cdd3d9e0ec7fcaa6",
+    "url": "/Alive/static/media/mountains.0e928193.jpg"
+  }
+]);
