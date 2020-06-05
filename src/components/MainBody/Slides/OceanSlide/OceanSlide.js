bb7cbe0300cb5f72b369bb5341ab8d05fae25974
VladokAR <vladokthrr@gmail.com>
2020-06-05 13:43:53 +0300

    ocean parallax effect done

--- src/components/MainBody/Slides/OceanSlide/OceanSlide.js
+++ src/components/MainBody/Slides/OceanSlide/OceanSlide.js
@@ -11,42 +11,85 @@ class OceanSlide extends Component {
         renderObjects: {
             ocean: {
                 name: "Ocean",
-                image: Ocean
+                image: Ocean,
+                speed: 0.1,
+                transform: "translateY(0px)"
             },
             reef: {
                 name: "Reef",
-                image: Reef
+                image: Reef,
+                speed: -0.2,
+                transform: "translateY(0px)"
             },
             seagirl: {
                 name: "SeaGirl",
-                image: SeaGirl
+                image: SeaGirl,
+                speed: 0.25,
+                transform: "translateY(0px)"
             },
             divers: {
                 name: "Divers",
-                image: Divers
+                image: Divers,
+                speed: -0.3,
+                transform: "translateY(0px)"
             },
             turtle: {
                 name: "Turtle",
-                image: Turtle
+                image: Turtle,
+                speed: -0.4,
+                transform: "translateY(0px)"
+            },
+            label: {
+                speed: -0.1,
+                transform: "translateY(0px)"
             }
         }
     }
 
+    componentDidMount() {
+        document.addEventListener("scroll", this.scrollHandler);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener("scroll", this.scrollHandler);
+    }
+
+    scrollHandler = () => {
+        let clientY = window.pageYOffset;
+        let elements = {...this.state.renderObjects};
+        Object.keys(elements).forEach((key) => {
+            let elYC = clientY * elements[key].speed;
+            elements[key].transform = "translateY(" + elYC + "px)";
+        });
+
+        this.setState({
+            renderObjects: elements
+        });
+    };
+
     render() {
         const renderObjects = this.state.renderObjects;
-        const slide = Object.keys(renderObjects).map((obj) => {
-            let name = renderObjects[obj].name;
-            let wrapper = classes["Wrapper_" + name];
-            let imgName = classes[name];
-            return (
-                <div key={obj} className={wrapper}>
-                    <img className={imgName} src={renderObjects[obj].image} alt={name}/>
-                </div>
-            );
+        const slide = Object.keys(renderObjects).map((key) => {
+            if (renderObjects[key].image !== undefined) {
+                let name = renderObjects[key].name;
+                let wrapper = classes["Wrapper_" + name];
+                let imgName = classes[name];
+                return (
+                    <div key={key} style={{transform: renderObjects[key].transform}} className={wrapper}>
+                        <img className={imgName} src={renderObjects[key].image} alt={name}/>
+                    </div>
+                );
+            } else {
+                return "";
+            }
         });
         return (
             <div className={classes.OceanSlide}>
                 {slide}
+                <div style={{transform: this.state.renderObjects["label"].transform}}  className={classes.Wrapper_IS}>
+                    <div className={classes.I_letter}>I</div>
+                    <div className={classes.S_letter}>S</div>
+                </div>
                 <div className={classes.FullSize}>
                 </div>
             </div>
